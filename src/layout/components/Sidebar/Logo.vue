3ea33497ba5f984eb2199778b5c37875ec4b33cb
e <2251710772@qq.com>
2020-10-22 22:16:27 +0800

    上传了代码

---
+++ src/layout/components/Sidebar/Logo.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+    <transition name="sidebarLogoFade">
+      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 v-else class="sidebar-title">{{ title }} </h1>
+      </router-link>
+      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 class="sidebar-title">{{ title }} </h1>
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: '乐乐云商店',
+      logo: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603383986218&di=3cdad8d08a98cebeeb4426bd5f686c14&imgtype=0&src=http%3A%2F%2Fhiphotos.baidu.com%2Ffeed%2Fpic%2Fitem%2F0e2442a7d933c895a0c9c0d2dd1373f0830200e7.jpg'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
+}
+
+.sidebar-logo-container {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
+
+  & .sidebar-logo-link {
+    height: 100%;
+    width: 100%;
+
+    & .sidebar-logo {
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+      margin-right: 12px;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      margin: 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 50px;
+      font-size: 14px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+      vertical-align: middle;
+    }
+  }
+
+  &.collapse {
+    .sidebar-logo {
+      margin-right: 0px;
+    }
+  }
+}
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="dashboard-container">
+ <el-timeline>
+    <el-timeline-item timestamp="2020/9/12" placement="top">
+      <el-card>
+        <h4>乐乐云商店</h4>
+        <p>店员1 于 2020/9/12 20:46 上货 </p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2020/9/15" placement="top">
+      <el-card>
+        <h4>乐乐云商店</h4>
+        <p>店员2 于 2020/9/15 20:46 补货</p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2018/4/2" placement="top">
+      <el-card>
+        <h4>乐乐云商店</h4>
+        <p>店员3 于 2020/9/18 20:46 统计了店销售额</p>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+  <el-tag>月销售额差评百分比</el-tag>
+<el-progress type="circle" :percentage="25"></el-progress>
+  <el-tag>月销售额完成度百分比</el-tag>
+<el-progress type="circle" :percentage="100" status="success"></el-progress>
+  <el-tag>月员工积极度百分比</el-tag>
+<el-progress type="circle" :percentage="70" status="warning"></el-progress>
+
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .is-selected {
+    color: #1989FA;
+  }
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
